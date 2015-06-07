58011ac7f52d3ef008546b3a674e5e68a4ac7b89
Renato Italo <renato.filizzola@gmail.com>
2015-06-07 15:22:55 -0300

    Refazendo o codigo usando a funca de ativacao sigmoide

--- main.py
+++ main.py
@@ -45,6 +45,9 @@ def parser_csv():
 		f.close()
 	return entrada , valor_esperado
 
+def sigmoid(x):
+  return 1 / (1 + math.exp(-x))
+  
 def cria_matriz(i,j):
   m = []
   for k in range(i):
@@ -55,30 +58,6 @@ def main():
 	entrada , valor_esperado = parser_csv()
 	neural_test = NeuralNetwork(12,9,1)
 	
-	print "Matriz de pesos da camada de entrada:"
-	print neural_test.peso_entrada
-	print "Matriz de pesos da camada escondida:"
-	print neural_test.peso_escondido
-	
-	grupo_de_teste = []
-	valor_esperado_teste = []
-	for i in range(100,200):
-		grupo_de_teste.append(entrada[i])
-		valor_esperado_teste.append(float(valor_esperado[i]))
-	
-	grupo_de_treinamento = []
-	valor_esperado_treinamento = []
-	for i in range(201,251):
-		grupo_de_treinamento.append(entrada[i])
-		valor_esperado_treinamento.append(float(valor_esperado[i]))
-	
-	neural_test.treino(grupo_de_treinamento,valor_esperado_treinamento,1000,0.5,0.005)
-	neural_test.teste(grupo_de_teste,valor_esperado_teste)
-	
-	print "Matriz de pesos da camada de entrada final:"
-	print neural_test.peso_entrada
-	print "Matriz de pesos da camada escondida final:"
-	print neural_test.peso_escondido    
 	
 	
 def gerar_pesos_aleatorios(matriz):
@@ -105,73 +84,21 @@ class NeuralNetwork:
 		self.peso_escondido = gerar_pesos_aleatorios(self.peso_escondido)
 		
 	def calc_y(self,entrada):
-		
-		self.matriz_entrada = entrada
-		#camada de entrada -> camada escondida
 		for i in range(self.nos_escondidos):
-			total = 0.0
 			for j in range(self.nos_entrada):
-				total += float(entrada[i]) * self.peso_entrada[j][i]
-			self.matriz_y_escondida[i] = tanh(total)
+
+
 		
-		#camada escondida -> camada de saida
-		for k in range(self.nos_saida):
-			total = 0.0
-			for j in range(self.nos_escondidos):
-				total += self.matriz_y_escondida[k] * self.peso_escondido[j][k]
-			self.matriz_y_saida = total
-			
-		return self.matriz_y_saida
 	  
 	def retroPropagacao(self,saida_esperada, N):
 		
-		#calculando erros (deltas) da saida
-		delta_saida = [0.0]*self.nos_saida
-		for i in range(self.nos_saida):
-			delta_saida[i] = saida_esperada - self.matriz_y_saida
 		
-		#atualizando pesos da camada escondida
-		for i in range(self.nos_escondidos):
-			for k in range(self.nos_saida):
-				self.peso_escondido[i][k] = self.peso_escondido[i][k] + N * delta_saida[k] * self.matriz_y_escondida[i]        	
-				
-		#calculando erros (deltas) da camada escondida
-		delta_escondido = [0.0]*self.nos_escondidos
-		for i in range(self.nos_escondidos):
-			erro = 0.0
-			for j in range(self.nos_saida):
-				erro += delta_saida[j] * self.peso_escondido[i][j]
-			delta_escondido[i] = dtanh(self.matriz_y_escondida[i]) * erro
-			
-		#atualizando pesos da camda de entrada
-		for i in range(self.nos_entrada):
-			for j in range(self.nos_escondidos):
-				self.peso_entrada[i][j] = self.peso_entrada[i][j] + N * delta_escondido[j] * float(self.matriz_entrada[i])
-				
-		#calcular o erro geral
-		erro = 0.0
-		for i in range(self.nos_saida):
-			erro += 0.5*((saida_esperada - self.matriz_y_saida)**2)
-		
-		return erro
 			
 	def treino(self,entrada,valor_esperado,epocas,N,erro_max):
-		erro = 0.0
-		for i in range(0,epocas):
-			for j in range(0,len(entrada)):
-				r = self.calc_y(entrada[j])
-				tmp = self.retroPropagacao(valor_esperado[j],N)
-				print "Erro: %f da iteração %d da época %d" %(tmp,j,i)
-
-				erro += tmp
-			if erro < erro_max:
-				print "Erro: %f" %erro
-				break
+		
 	
 	def teste(self,entrada,valor_esperado):
-		for i in range(len(entrada)):
-			y = self.calc_y(entrada[i])
-			print "Valor esperado: %f || Valor obtido: %f" %(valor_esperado[i],y)
+		
 	  
 if __name__ == '__main__':
 	main()
